b0a5dcf908c3d4e63c3d077d3bf3d082fe537094
fakestarbaby <fakestarbaby@gmail.com>
2011-07-27 20:08:24 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRailsAdmin::Application.config.secret_token = 'c4180f33bfb21910dd0f46577a507fdb5e10bf19f09004be188b1bb72fac7bd6ed131a1876762f9ffa68b611d2722b7dece73030eb2ffbbc7427e0e0a67fde8f'
